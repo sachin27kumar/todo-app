3eaef8f63097386ddade4ce32164797db3d4f4d9
sachin27kumar <sachin27sheoran@gmail.com>
2024-11-07 12:27:44 +0530

    reinstalled npm package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default {
-  base: '/todo-app/', 
-}
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/todo-app/', // Adjust to your repo name, if it is "todo-app"
+});
+
